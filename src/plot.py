5c1d34d3f1bdbba088e1f8adf3ed205a70a5f561
David Makeev <dmakeev@intermedia.com>
2025-02-03 10:49:29 +0000

    csv delimiters

--- src/plot.py
+++ src/plot.py
@@ -4,7 +4,7 @@ def plot_intersections(intersections: dict):
     times = sorted(intersections.keys())
     intersection_counts = [len(intersections[time]) for time in times]
 
-    plt.figure(figsize=(10, 5))
+    plt.figure(figsize=(15, 8))
     plt.plot(times, intersection_counts, marker='o', color='b', linestyle='-')
     plt.title("Molecule Intersections Over Time")
     plt.xlabel("Time")
